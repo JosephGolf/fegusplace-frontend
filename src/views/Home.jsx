26bfcbe2d992205e5ac3c68d63b362f5162d5aeb
josephnma <josgolf@yahoo.ca>
2023-11-24 23:56:12 +0100

    edited app.css

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -238,7 +238,7 @@ class HomeViewClass extends Component {
             </div>
             <div className="col-md-8">
               <div className="card p-2">
-                <div className="card-header bg-warning">
+                <div className="card-header btn-warning">
                   <h4>{this.props.trans('superMarket')}</h4>
                 </div>
                 <Slider data={this.state.suerpMarkets} />
