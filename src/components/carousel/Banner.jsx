81055e74c5f1c3812bbd97be7fe53e4600f32bac
mikeadesina <michaeladeshina015@gmail.com>
2023-11-25 21:58:22 +0100

    fix banner

--- src/components/carousel/Banner.jsx
+++ src/components/carousel/Banner.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './banner.css';
 
 const Item = ({ item, index }) => (
     <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
