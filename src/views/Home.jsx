b9e92f23612d28292cb51d9c735aea765f947cb0
josephnma <josgolf@yahoo.ca>
2023-11-29 17:02:03 +0100

    edited supermarket card colour

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -237,7 +237,7 @@ class HomeViewClass extends Component {
             </div>
             <div className="col-md-8">
               <div className="card p-2">
-                <div className="card-header" style={{ backgroundColor: 'blue' }}>
+                <div className="card-header" style={{ backgroundColor: '#000080' }}>
                   <h4>{this.props.trans('superMarket')}</h4>
                 </div>
                 <Slider data={this.state.suerpMarkets} />
