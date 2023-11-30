022312c07020c5d66cad1b20aefdec93618779c7
josephnma <josgolf@yahoo.ca>
2023-11-30 11:48:55 +0100

    redited home ad image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -163,6 +163,7 @@ class HomeViewClass extends Component {
 
             </div>
 
+            <div className="col-md-2">
               <div className="card p-3">
                 <div className="markup -pvs">
                   <img src="https://res.cloudinary.com/ddsfhaly2/image/upload/v1700857063/Green_and_White_Conceptual_New_Season_Billboard_miwpcn.png"
@@ -170,6 +171,7 @@ class HomeViewClass extends Component {
                        style={{ maxWidth: '100%', maxHeight: '3in', width: '100%', height: 'auto' }}/>
                 </div>
               </div>
+            </div>
 
           </div>
         </div>
